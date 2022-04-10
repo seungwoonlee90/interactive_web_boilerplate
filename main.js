33b79da2adb0c26191a5390c72daf1cf9c4a7138
ethanlee <seungwoonlee90@gmail.com>
2022-04-10 12:46:23 +0900

    currentScene check

--- main.js
+++ main.js
@@ -46,6 +46,16 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
+
+        let totalScrollHeight = 0;
+        for (let i=0; i<sceneInfo.length; i++) {
+            totalScrollHeight = totalScrollHeight + sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute("id", `show-scene-${currentScene}`)
     };
 
     function scrollLoop() {
@@ -55,21 +65,22 @@
         }
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
             currentScene++;
+            document.body.setAttribute("id", `show-scene-${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
             if(currentScene === 0) return
             currentScene--;
+            document.body.setAttribute("id", `show-scene-${currentScene}`);
         }
-
     }
 
-    window.addEventListener("resize", setLayout);
     window.addEventListener("scroll", ()=>{
         yOffset = window.pageYOffset;
         scrollLoop();
     });
 
-    setLayout();
+    window.addEventListener("load", setLayout);
+    window.addEventListener("resize", setLayout);
 
 })();
